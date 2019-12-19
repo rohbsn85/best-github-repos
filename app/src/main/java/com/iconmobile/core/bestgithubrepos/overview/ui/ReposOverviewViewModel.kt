316a2f8ffb7d23b78475419a8b42f10ff07dd114
Robert Kerz <robert.kerz@iconmobile.com>
2019-12-19 09:29:35 +0100

    integrate repository in ViewModel

--- app/src/main/java/com/iconmobile/core/bestgithubrepos/overview/ui/ReposOverviewViewModel.kt
+++ app/src/main/java/com/iconmobile/core/bestgithubrepos/overview/ui/ReposOverviewViewModel.kt
@@ -1,13 +1,13 @@
 package com.iconmobile.core.bestgithubrepos.overview.ui
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
+import androidx.lifecycle.liveData
+import com.iconmobile.core.bestgithubrepos.overview.repository.GitHubRepoRepository
+import kotlinx.coroutines.Dispatchers
 
-class ReposOverviewViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ReposOverviewViewModel(private val repository: GitHubRepoRepository) : ViewModel() {
 
-    fun fetchRepos() {
-        viewModelScope.launch {  }
+    val repoList = liveData(Dispatchers.IO) {
+        emit(repository.getRepoOverview())
     }
 }
